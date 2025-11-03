92098bc88d4c96311ee951cbe42d5d2b93177b6d
bleudev <aitiiigg1@gmail.com>
2025-11-03 18:12:29 +0300

    Update rendering lobby button

--- 1.21.9/src/main/java/com/bleudev/ppl_utils/mixin/client/GameMenuScreenMixin.java
+++ 1.21.9/src/main/java/com/bleudev/ppl_utils/mixin/client/GameMenuScreenMixin.java
@@ -1,18 +1,18 @@
 package com.bleudev.ppl_utils.mixin.client;
 
 import com.bleudev.ppl_utils.client.compat.modmenu.PplUtilsConfig;
-import com.bleudev.ppl_utils.client.impl.LobbyButtonText;
 import com.bleudev.ppl_utils.util.ServerUtils;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.tooltip.Tooltip;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextIconButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.bleudev.ppl_utils.util.RegistryUtils.getIdentifier;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
@@ -23,18 +23,18 @@ public abstract class GameMenuScreenMixin extends Screen {
     // Rendering
     @Unique
     private void drawLobbyButton() {
-        var b = ButtonWidget
-            .builder(Text.object(LobbyButtonText.INSTANCE), button ->
-                ServerUtils.executeCommand("lobby"))
-            .dimensions(this.width / 2 - 125, this.height / 4 + 32, 20, 20);
-        if (PplUtilsConfig.lobby_button_tooltip_enabled) b = b
-            .tooltip(Tooltip.of(Text.translatable("text.ppl_utils.game_menu.lobby_button.tooltip")));
-
-        this.addDrawableChild(b.build());
+        var b = TextIconButtonWidget.builder(Text.translatable("text.ppl_utils.game_menu.lobby_button.tooltip"),
+            button -> ServerUtils.executeCommand("lobby"), true)
+            .texture(getIdentifier("pepe_mono"), 13, 13)
+            .dimension(20, 20);
+        if (PplUtilsConfig.lobby_button_tooltip_enabled) b = b.useTextAsTooltip();
+        var btn = b.build();
+        btn.setPosition(this.width / 2 - 125, this.height / 4 + 32);
+        this.addDrawableChild(btn);
     }
 
     @Inject(method = "initWidgets", at = @At("RETURN"))
     private void addLobbyButton(CallbackInfo ci) {
-        if (PplUtilsConfig.lobby_button_enabled && ServerUtils.isClientOnServerSupportsLobbyCommand()) drawLobbyButton();
+        if (PplUtilsConfig.lobby_button_enabled) drawLobbyButton();
     }
 }
