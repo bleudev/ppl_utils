6752d6469157f89fc1e89a07f600242ee7f99ccd
bleudev <aitiiigg1@gmail.com>
2025-11-13 17:08:06 +0300

    [1.21.9] Boss bar. Need to get message with restart time

--- 1.21.9/src/main/java/com/bleudev/ppl_utils/client/PplUtilsClient.java
+++ 1.21.9/src/main/java/com/bleudev/ppl_utils/client/PplUtilsClient.java
@@ -5,10 +5,13 @@ import com.bleudev.ppl_utils.client.custom.Keys;
 import com.bleudev.ppl_utils.client.custom.debug.WorldBorderDebugHudEntry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.debug.DebugHudEntries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
 
 import static com.bleudev.ppl_utils.PplUtilsConst.*;
 import static com.bleudev.ppl_utils.client.ClientCallbacks.executeLobby;
@@ -18,6 +21,7 @@ import static net.minecraft.SharedConstants.TICKS_PER_MINUTE;
 
 public class PplUtilsClient implements ClientModInitializer {
     int beta_mode_message_ticks;
+    private RestartHelper restartHelper;
 
     @Override
     public void onInitializeClient() {
@@ -25,6 +29,7 @@ public class PplUtilsClient implements ClientModInitializer {
         Keys.initialize();
 
         beta_mode_message_ticks = 0;
+        restartHelper = new RestartHelper();
 
         LOGGER.debug("Register {} debug hud entry", getIdentifier("world_border"));
         DebugHudEntries.register(getIdentifier("world_border"), new WorldBorderDebugHudEntry());
@@ -46,6 +51,23 @@ public class PplUtilsClient implements ClientModInitializer {
             if (beta_mode_message_ticks > 0) beta_mode_message_ticks--;
 
             while (Keys.LOBBY_KEY.wasPressed()) executeLobby(client);
+
+            updateBossBar(client);
         });
+        ClientReceiveMessageEvents.CHAT.register((text, signedMessage, gameProfile, parameters, instant) -> {
+            var content = text.getString().replaceAll("<[^< >]+> *", "");
+            try {
+                LOGGER.debug("run boss bar {}", content);
+                RestartHelper.runRestartBar(Long.parseLong(content));
+            } catch (NumberFormatException ignored) {
+                LOGGER.error("Number format exception: {}", content);
+            }
+        });
+    }
+
+
+    private void updateBossBar(@NotNull MinecraftClient client) {
+        if (client.player == null) return;
+        restartHelper.update(client);
     }
 }
